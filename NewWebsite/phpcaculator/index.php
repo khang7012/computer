b5d4a99c8fd1ba380af4f2f56bc8c9987897aaf9
khang7012 <55459596+khang7012@users.noreply.github.com>
2020-03-04 12:26:30 +0700

    Add files via upload

---
+++ NewWebsite/phpcaculator/index.php
@@ -0,0 +1,64 @@
+<?php
+$smg = "";
+if(isset($_POST['btn'])) {
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $calc = $_POST['calc'];
+
+    if ($calc == "1") {
+        $smg = '<h1>'.$result = $num1 + $num2.'</h1>';
+    } elseif ($calc == "2") {
+        $smg =  'h1'.$result = $num1 - $num2.'</h1>';
+    } elseif ($calc == "3") {
+        $smg = '<h1>'.$result = $num1 * $num2.'</h1>'
+    } elseif ($calc == "4") {
+        $smg = '<h1>'.$result = $num1 / $num2.'</h1>'
+    }
+}
+?>
+<!DOCTYPE html>
+<html >
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<form>
+    <?php echo $smg; ?>
+    <input type="text" name="num1" placeholder="Number 1">
+    <input type="text" name="num2" placeholder="Number 2">
+
+    <select name="operator" title="khang">
+        <option>None</option>
+        <option>Subtract</option>
+        <option>Add</option>
+        <option>Multiply</option>
+        <option>Divide</option>
+    </select>
+    <br>
+    <button type="submit" name="submit" value="submit">Calculate</button>
+</form>
+
+<?php
+if(isset($_GET['submit'])){
+    switch($operator) {
+        case "None":
+            echo "Select Operator";
+            break;
+        case  "Add":
+            echo $result1 + $result2;
+            break;
+        case "Subtract":
+            echo "$result1" - $result2;
+            break;
+        case "Multiply":
+            echo $result1 * $result2;
+            break;
+        case "Divide":
+            echo $result1 / $result2;
+            break;
+    }
+}
+?>
+</body>
+</html>
